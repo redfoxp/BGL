23c2dd4ae39e88d65f80065d417425f779ad6aec
Marius Ghertescu <m.ghertescu@gmail.com>
2019-11-14 11:47:14 +0100

    dev avance

--- src/hello.c
+++ src/hello.c
@@ -2,6 +2,7 @@
 
 int main(int argc, char const *argv[]) {
   printf("%s\n", "Hello World");
-  printf("%s\n", "Typed from branch dev but changed after merge");
+  printf("%s\n", "Typed from branch dev");
+  printf("%s\n","dev prend de l'avance sur master" );
   return 0;
 }

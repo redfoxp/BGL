92a62e2895f8cbcda354a1edb4ba9e57ec5cfef2
Marius Ghertescu <m.ghertescu@gmail.com>
2019-11-14 11:55:30 +0100

    dev avance+3

--- src/hello.c
+++ src/hello.c
@@ -5,5 +5,6 @@ int main(int argc, char const *argv[]) {
   printf("%s\n", "Typed from branch dev");
   printf("%s\n","dev prend de l'avance sur master" );
   printf("%s\n", "dev +2");
+  printf("%s\n", "dev +3");
   return 0;
 }

060365b423b3c418a25eab90e8b6e02e2d135a82
Marius Ghertescu <m.ghertescu@gmail.com>
2019-11-14 11:41:29 +0100

    modified print after merge

--- src/hello.c
+++ src/hello.c
@@ -2,6 +2,6 @@
 
 int main(int argc, char const *argv[]) {
   printf("%s\n", "Hello World");
-  printf("%s\n", "Typed from branch dev");
+  printf("%s\n", "Typed from branch dev but changed after merge");
   return 0;
 }
